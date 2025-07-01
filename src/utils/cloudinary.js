c17b32003fe696fb734f7cdfbe33d9dc05d1328c
YEDASAVG <abhirajd2012@gmail.com>
2025-07-01 08:48:01 +0530

    Project configuration complete

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import { console } from "inspector";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfully
+    console.log("file is uploaded on cloudinary", response.url);
+    return response;
+  } catch (error) {
+    try {
+      fs.unlinkSync(localFilePath); // remove the locally saved temp files as the upload operation gets failed
+    } catch (unlinkErr) {
+      console.error("Failed to delete lcoal file:", unlinkErr);
+    }
+    console.error("cloudinary upload failed:", error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
